3cf5a711bb48835991be286f320b76c31b412f6b
Weboscopy <weboscopy@gmail.com>
2025-10-24 23:04:19 +0200

    fixing snap-center

--- components/Home/Contact.tsx
+++ components/Home/Contact.tsx
@@ -155,7 +155,7 @@ function ContactForm() {
 function Contact() {
     const t = useScopedI18n("Home.Contact")
     return (
-        <section className="h-screen snap-start ">
+        <section className="h-screen snap-center">
             <div className="sticky z-40 overflow-hidden top-0 left-0 bg-background pt-20 md:pt-15 pb-4 mb-10 text-center flex flex-col items-center">
                 <ScrollFloat
                     animationDuration={1}
