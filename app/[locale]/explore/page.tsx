45eeddf4e4ed69470f0c8c07caba3a514cf4435a
Weboscopy <weboscopy@gmail.com>
2025-11-03 22:22:45 +0100

    page

--- app/[locale]/explore/page.tsx
+++ app/[locale]/explore/page.tsx
@@ -10,7 +10,7 @@ import Separator from "@/components/ui/Separator";
 
 
 
-export function ExplorePage() {
+export default function ExplorePage() {
     const t = useScopedI18n("Explore")
     const headerImages = [
         "https://images.unsplash.com/photo-1446038202205-1c96430dbdab?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1169",
@@ -138,4 +138,3 @@ export function ExplorePage() {
         </>
     )
 }
-export default ExplorePage
